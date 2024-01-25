f67d89a2d91080ea478d3886ac270c1207d5c71e
TshifhiwaMuade <157051586+TshifhiwaMuade@users.noreply.github.com>
2024-01-25 20:35:21 +0200

    initial commit
    initial commit

---
+++ src/app/body/body.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html',
+  styleUrl: './body.component.css'
+})
+export class BodyComponent implements OnInit{
+  onSubmit(){
+    //this.userForm.updateValueAndValidity();
+    console.log("Hello world");
+    
+
+  }
+    
+
+  constructor(
+    private fb: UntypedFormBuilder
+  ){
+
+  }
+  ngOnInit(){
+    // this.userForm = this.fb.group({ 
+    //   userName : ['',Validators.required] ,
+    //   password : ['',Validators.required]
+    //  });
+    
+    
+  }
+}
