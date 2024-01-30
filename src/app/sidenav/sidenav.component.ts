b4ef0f7273ec82696ee55cc09191cfbd7f17aa94
TshifhiwaMuade <157051586+TshifhiwaMuade@users.noreply.github.com>
2024-01-30 03:01:04 +0200

    commit

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,32 +1,63 @@
-import { Component, Output , EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit, HostListener } from '@angular/core';
 import { navbarData } from './nav-data';
+import { animate, keyframes, style, transition, trigger } from '@angular/animations';
 
-
-interface SideNavToggle{
-  screenWidth:number;
+interface SideNavToggle {
+  screenWidth: number;
   collapsed: boolean;
 }
+
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrl: './sidenav.component.css'
+  styleUrls: ['./sidenav.component.css'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('350ms', style({ opacity: 1 }))
+      ]),
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate('350ms', style({ opacity: 0 }))
+      ])
+    ]),
+    trigger('rotate', [
+      transition(':enter', [
+        animate('1000ms', keyframes([
+          style({ transform: 'rotate(0deg)', offset: '0' }),
+          style({ transform: 'rotate(2turn)', offset: '1' })
+        ]))
+      ])
+    ])
+  ]
 })
-
-export class SidenavComponent {
-
+export class SidenavComponent implements OnInit {
   @Output() onToggleSideNav: EventEmitter<SideNavToggle> = new EventEmitter();
   collapsed = false;
-  screenWidth!: 0;
+  screenWidth = 0;
+
   navData = navbarData;
 
-
-  toggleCollapse(): void{
-    this.collapsed = !this.collapsed;
-    this.onToggleSideNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.screenWidth = window.innerWidth;
+    if (this.screenWidth <= 768) {
+      this.onToggleSideNav.emit({ collapsed: this.collapsed, screenWidth: this.screenWidth });
+    }
   }
-closesSidenav(): void{
-    this.collapsed = false
-    this.onToggleSideNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
 
-}
+  ngOnInit(): void {
+    this.screenWidth = window.innerWidth;
+  }
+
+  toggleCollapse(): void {
+    this.collapsed = !this.collapsed;
+    this.onToggleSideNav.emit({ collapsed: this.collapsed, screenWidth: this.screenWidth });
+  }
+
+  closesSidenav(): void {
+    this.collapsed = false;
+    this.onToggleSideNav.emit({ collapsed: this.collapsed, screenWidth: this.screenWidth });
+  }
 }
